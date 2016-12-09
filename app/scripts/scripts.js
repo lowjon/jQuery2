329e30382baa4b82cd67e3798ee4962b793ec306
Jon Low <lowjonc@gmail.com>
2016-12-08 20:48:09 -0700

    localStorage retreive preogress -- need to do something with the listo array now

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,23 +1,44 @@
 $(document).ready(function(){
-
+  // localStorage.clear();
 $('#newTaskForm').hide();
+
 console.log(localStorage);
 
-// if(localStorage && localStorage.getItem('listo')){
-//   localStorage.getItem('listo');
-//   JSON.parse('listo');
-//   var listo = listo;
-// } else{
+if(localStorage && localStorage.getItem('listo')){
+  var listo = JSON.parse(localStorage.getItem('listo'));
+  localStorage.clear();
+} else{
+    listo = [];
+    localStorage.clear();
+};
 
-    var listo = [];
-// };
-
-
-// localStorage.clear();
+console.log(listo);
 
 var Task = function(task){
   this.task = task;
   this.id = 'new';
+};
+
+var reloadTheStuff = function(listo){
+  if (listo){
+    for (var i = 0; i < listo.length; i++) {
+      listo[i].loadStuff();
+    }
+  }
+};
+
+var loadStuff = function(){
+  $('#newList').append(
+    '<a href= "#finish" class="" id= "item">'+
+    '<li class="list-group-item">'+
+    '<h3>' + task.task +'</h3>'+
+    '<span class="arrow pull-right">'+
+    '<i class="glyphicon glyphicon-arrow-right">' +
+    '</span>'+
+    '</li>' +
+    '</a>'
+  );
+
 };
 
 var addTask = function (task) {
@@ -41,22 +62,6 @@ var addTask = function (task) {
   $('#newTaskForm').slideToggle('fast','linear');
 };
 
-
-$('#saveNewItem').on('click', function(e){
-  e.preventDefault();
-  var task = $('#newItemInput').val().trim();
-  addTask(task);
-});
-
-$('#add-todo').on('click', function () {
-  $('#newTaskForm').fadeToggle('fast', 'linear');
-});
-
-$('#cancel').on('click', function (e) {
-  e.preventDefault();
-  $('#newTaskForm').fadeToggle('fast', 'linear');
-});
-
 var advanceTask = function (task) {
   var modified = task.innerText.trim().toLowerCase();
   for (var i = 0; i < listo.length; i++) {
@@ -74,6 +79,26 @@ var advanceTask = function (task) {
   }
   task.remove();
 };
+
+
+$(window).load(listo, reloadTheStuff());
+
+
+$('#saveNewItem').on('click', function(e){
+  e.preventDefault();
+  var task = $('#newItemInput').val().trim();
+  addTask(task);
+});
+
+$('#add-todo').on('click', function () {
+  $('#newTaskForm').fadeToggle('fast', 'linear');
+  $('#newItemInput').focus();
+});
+
+$('#cancel').on('click', function (e) {
+  e.preventDefault();
+  $('#newTaskForm').fadeToggle('fast', 'linear');
+});
 
 $(document).on('click', '#item', function (e) {
   e.preventDefault();
@@ -105,8 +130,12 @@ $('#newItemInput').bind('keypress', function(e){
  }
 });
 
-// $(window).unload(function () {
-//   localStorage.setItem('listo', JSON.stringify(listo));
-// })
+console.log(listo);
+
+$(window).unload(function () {
+  localStorage.setItem('listo', JSON.stringify(listo));
+});
+
+console.log(localStorage);
 
 });
